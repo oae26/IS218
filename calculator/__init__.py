c7cd4c61754d163c1a18c386c517c3d02f24b203
oae26 <oae26@njit.edu>
2024-09-18 09:50:33 -0400

    updated files

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -3,3 +3,9 @@
 def add(a,b):
     """adds two numbers"""
     return a + b
+def subtract(a,b):
+    """subtracts two numbers"""
+    return a - b
+def divide(a,b):
+    """divides two numbers"""
+    return a / b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,6 +2,14 @@
 Provides some arithmetic Functions
 """
 from calculator import add
+from calculator import subtract
+from calculator import divide
 def test_addition():
     """Tests addition"""
     assert add(2,2)
+def test_subtraction():
+    """Tests the subtraction of two numbers"""
+    assert subtract(2,2) == 0
+def test_division():
+    """Tests the division of two numbers"""
+    assert divide(2,2)
