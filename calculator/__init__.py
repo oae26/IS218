160a707ff0be326eb33b072f43ecc5edf87b86d2
oae26 <oae26@njit.edu>
2024-09-18 09:30:48 -0400

    added all new files

---
+++ calculator/__init__.py
@@ -0,0 +1,5 @@
+"""Provides an arithmetic addition function"""
+
+def add(a,b):
+    """adds two numbers"""
+    return a + b
---
+++ tests/test_calculator.py
@@ -0,0 +1,7 @@
+"""
+Provides some arithmetic Functions
+"""
+from calculator import add
+def test_addition():
+    """Tests addition"""
+    assert add(2,2)
